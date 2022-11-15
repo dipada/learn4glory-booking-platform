f6617edb75e3119cc30855228c900345f321117c
dipada <daniele.dipalma68@gmail.com>
2022-11-15 16:24:40 +0100

    added BookedLessonDAO concrete implementation

---
+++ src/main/java/com/tweb/dpd/learn4glory/dao/mysql/BookedLessonDAOImpMySql.java
@@ -0,0 +1,203 @@
+package com.tweb.dpd.learn4glory.dao.mysql;
+
+import com.tweb.dpd.learn4glory.dao.BookedLessonDAO;
+import com.tweb.dpd.learn4glory.model.BookedLesson;
+import com.tweb.dpd.learn4glory.model.WEEK_DAY;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookedLessonDAOImpMySql implements BookedLessonDAO {
+
+  private final String QUERY_INSERT_BOOKEDLESSON = "INSERT INTO BOOKED(USER, LESSON, WEEK_DAY, HOUR, COMPLETED, DELETED) VALUES (?,?,?,?,?,?)";
+  private final String QUERY_UPDATE_COMPLETED = "UPDATE BOOKED SET COMPLETED=TRUE WHERE ID_BOOKING=?";
+  private final String QUERY_UPDATE_DELETED = "UPDATE BOOKED SET DELETED=TRUE WHERE ID_BOOKING=?";
+  private final String QUERY_SELECT_BOOKEDLESSON_BYID = "SELECT * FROM BOOKED WHERE ID_BOOKING=?";
+  private final String QUERY_SELECT_ALL_BOOKEDLESSON = "SELECT * FROM BOOKED";
+  private final String QUERY_SELECT_ALL_BOOKEDLESSON_USERID = "SELECT * FROM BOOKED WHERE USER=?";
+  private final String QUERY_SELECT_ALL_ACTIVE_BOOKEDLESSON = "SELECT * FROM BOOKED WHERE COMPLETED=FALSE AND DELETED=FALSE";
+
+  @Override
+  public int insertBookedLesson(BookedLesson bookedLesson) {
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+    int result = -1;
+
+    if (conn != null) {
+      try {
+        ps = conn.prepareStatement(QUERY_INSERT_BOOKEDLESSON, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, bookedLesson.getUser());
+        ps.setInt(2, bookedLesson.getLesson());
+        ps.setString(3, bookedLesson.getWeek_day().toString());
+        ps.setString(4, String.valueOf(bookedLesson.getHour()));
+        ps.setBoolean(5, bookedLesson.isCompleted());
+        ps.setBoolean(6, bookedLesson.isDeleted());
+        ps.executeUpdate();
+        rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+          result = rs.getInt(1);
+        }
+        rs.close();
+        ps.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public BookedLesson selectBookedLessonById(int id_bookedLesson) {
+    PreparedStatement ps;
+    ResultSet rs;
+    BookedLesson bookedLesson = null;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        ps = conn.prepareStatement(QUERY_SELECT_BOOKEDLESSON_BYID);
+        ps.setInt(1,id_bookedLesson);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+          bookedLesson = new BookedLesson(rs.getInt(1), rs.getInt(2), rs.getInt(3), WEEK_DAY.valueOf(rs.getString(4)), Integer.parseInt(rs.getString(5)), rs.getBoolean(6), rs.getBoolean(7));
+        }
+        rs.close();
+        ps.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return bookedLesson;
+  }
+
+  @Override
+  public boolean markLessonCompleted(int id_bookedLesson) {
+    PreparedStatement ps;
+    boolean res = false;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        ps = conn.prepareStatement(QUERY_UPDATE_COMPLETED, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, id_bookedLesson);
+        res = ps.executeUpdate() > 0;
+        ps.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return res;
+  }
+
+  @Override
+  public boolean markLessonDeleted(int id_bookedLesson) {
+    PreparedStatement ps;
+    boolean res = false;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        ps = conn.prepareStatement(QUERY_UPDATE_DELETED, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, id_bookedLesson);
+        res = ps.executeUpdate() > 0;
+        ps.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return res;
+  }
+
+  @Override
+  public List<BookedLesson> selectAllBookedLessons() {
+    Statement st;
+    ResultSet rs;
+    List<BookedLesson> bookedLessons = null;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        st = conn.createStatement();
+        rs = st.executeQuery(QUERY_SELECT_ALL_BOOKEDLESSON);
+        bookedLessons = new ArrayList<>();
+        while (rs.next()) {
+          BookedLesson bookedLesson = new BookedLesson(rs.getInt(1), rs.getInt(2), rs.getInt(3), WEEK_DAY.valueOf(rs.getString(4)), Integer.parseInt(rs.getString(5)), rs.getBoolean(6), rs.getBoolean(7));
+          bookedLessons.add(bookedLesson);
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return bookedLessons;
+  }
+
+  @Override
+  public List<BookedLesson> selectAllBookedLessonsOfUser(int id_user) {
+    PreparedStatement ps;
+    ResultSet rs;
+    List<BookedLesson> bookedLessons = null;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        ps = conn.prepareStatement(QUERY_SELECT_ALL_BOOKEDLESSON_USERID);
+        ps.setInt(1, id_user);
+        rs = ps.executeQuery();
+        bookedLessons = new ArrayList<>();
+        while (rs.next()) {
+          BookedLesson bookedLesson = new BookedLesson(rs.getInt(1), rs.getInt(2), rs.getInt(3), WEEK_DAY.valueOf(rs.getString(4)), Integer.parseInt(rs.getString(5)), rs.getBoolean(6), rs.getBoolean(7));
+          bookedLessons.add(bookedLesson);
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return bookedLessons;
+  }
+
+  @Override
+  public List<BookedLesson> selectAllActiveBookedLessons() {
+    Statement st;
+    ResultSet rs;
+    List<BookedLesson> bookedLessons = null;
+    Connection conn = DAOFactoryMySql.openConnectionToDb();
+
+    if (conn != null) {
+      try {
+        st = conn.createStatement();
+        rs = st.executeQuery(QUERY_SELECT_ALL_ACTIVE_BOOKEDLESSON);
+        bookedLessons = new ArrayList<>();
+        while (rs.next()) {
+          BookedLesson bookedLesson = new BookedLesson(rs.getInt(1), rs.getInt(2), rs.getInt(3), WEEK_DAY.valueOf(rs.getString(4)), Integer.parseInt(rs.getString(5)), rs.getBoolean(6), rs.getBoolean(7));
+          bookedLessons.add(bookedLesson);
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+      } finally {
+        DAOFactoryMySql.closeDbConnection(conn);
+      }
+    }
+    return bookedLessons;
+  }
+}
--- src/main/java/com/tweb/dpd/learn4glory/dao/mysql/DAOFactoryMySql.java
+++ src/main/java/com/tweb/dpd/learn4glory/dao/mysql/DAOFactoryMySql.java
@@ -11,88 +11,96 @@ import java.sql.SQLException;
  */
 public class DAOFactoryMySql implements DAOFactory {
 
-  private static String url = null;
-  private static String user = null;
-  private static String password = null;
+    private static String url = null;
+    private static String user = null;
+    private static String password = null;
 
-  public DAOFactoryMySql(String urlDb, String userDb, String pwd) {
-    synchronized (this) {
-      url = urlDb;
-      user = userDb;
-      password = pwd;
+    public DAOFactoryMySql(String urlDb, String userDb, String pwd) {
+        synchronized (this) {
+            url = urlDb;
+            user = userDb;
+            password = pwd;
+        }
+        registerDriver();
     }
-    registerDriver();
-  }
 
-  private static void registerDriver() {
-    try {
-      DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-    } catch (SQLException e) {
-      e.printStackTrace();
-      System.out.println(e.getMessage());
+    private static void registerDriver() {
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
-  }
 
-  /**
-   * Open connection to db
-   *
-   * @return Connection obj if successful, null otherwise
-   */
-  protected static Connection openConnectionToDb() {
-    Connection conn = null;
-    try {
-      conn = DriverManager.getConnection(url, user, password);
-    } catch (SQLException e) {
-      e.printStackTrace();
-      System.out.println(e.getMessage());
+    /**
+     * Open connection to db
+     *
+     * @return Connection obj if successful, null otherwise
+     */
+    protected static Connection openConnectionToDb() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
-    return conn;
-  }
 
-  /**
-   * Close passed to db connection
-   */
-  protected static void closeDbConnection(Connection connection) {
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-      System.out.println(e.getSQLState());
-      e.printStackTrace();
+    /**
+     * Close passed to db connection
+     */
+    protected static void closeDbConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+            e.printStackTrace();
+        }
     }
-  }
 
-  /**
-   * @return concrete mysql implementation for userDao class
-   */
-  @Override
-  public UserDAO getUserDAO() {
-    return new UserDAOImplMySql();
-  }
+    /**
+     * @return concrete mysql implementation for userDao class
+     */
+    @Override
+    public UserDAO getUserDAO() {
+        return new UserDAOImplMySql();
+    }
 
-  /**
-   * @return concrete mysql implementation for teacherDao class
-   */
-  @Override
-  public TeacherDAO getTeacherDAO() {
-    return new TeacherDAOImplMySql();
-  }
+    /**
+     * @return concrete mysql implementation for teacherDao class
+     */
+    @Override
+    public TeacherDAO getTeacherDAO() {
+        return new TeacherDAOImplMySql();
+    }
 
-  /**
-   * @return concrete mysql implementation for courseDao class
-   */
-  @Override
-  public CourseDAO getCourseDAO() {
-    return new CourseDAOImplMySql();
-  }
+    /**
+     * @return concrete mysql implementation for courseDao class
+     */
+    @Override
+    public CourseDAO getCourseDAO() {
+        return new CourseDAOImplMySql();
+    }
 
-  /**
-   * @return concrete mysql implementation for lessonDAO class
-   */
-  @Override
-  public LessonDAO getLessonDAO() {
-    return new LessonDAOImplMySql();
-  }
+    /**
+     * @return concrete mysql implementation for lessonDAO class
+     */
+    @Override
+    public LessonDAO getLessonDAO() {
+        return new LessonDAOImplMySql();
+    }
+
+    /**
+     * @return concrete mysql implementation for BookedLessonDAO class
+     */
+    @Override
+    public BookedLessonDAO getBookedLessonDAO() {
+        return new BookedLessonDAOImpMySql();
+    }
 }
