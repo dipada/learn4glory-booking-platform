3578343abea26019836e205084b3ce435d91e0b4
dipada <daniele.dipalma68@gmail.com>
2023-01-11 02:41:06 +0100

    added hamburger menu animation to logged view navbar

--- src/main/webapp/resources/script.js
+++ src/main/webapp/resources/script.js
@@ -1,12 +1,26 @@
 function openHamburgerMenu() {
     var x = document.getElementById("navMenu");
+    var y = document.getElementById("navMenuLogged");
     var r = document.getElementById("icn");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-        r.className = "fa fa-bars";
+
+    if (x != null) {
+
+        if (x.style.display === "block") {
+            x.style.display = "none";
+            r.className = "fa fa-bars";
+        } else {
+            x.style.display = "block";
+            r.className += " fa-spin";
+        }
     } else {
-        x.style.display = "block";
-        r.className += " fa-spin";
+
+        if (y.style.display === "block") {
+            y.style.display = "none";
+            r.className = "fa fa-bars";
+        } else {
+            y.style.display = "block";
+            r.className += " fa-spin";
+        }
     }
 }
 
